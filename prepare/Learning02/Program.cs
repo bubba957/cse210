8cd01bdd80940acd93e05861d2d928d90e5b9685
bubba957 <scody49@gmail.com>
2024-09-24 22:19:35 -0700

    w02: prepare

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        job1._company = "Microsft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2016;
+        job1._endYear = 2018;
+
+        job2._company = "Bing";
+        job2._jobTitle = "IT";
+        job2._startYear = 2018;
+        job2._endYear = 2024;
+
+        job1.Display();
+        job2.Display();
+
+        Resume resume = new Resume();
+
+        resume._name = "Cody Smith";
+
+        // resume._jobs.Add(job1);
+        // resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
---
+++ prepare/Learning02/resume.cs
@@ -0,0 +1,16 @@
+using System;
+public class Resume
+{
+    public List<Job> _jobs = new List<Job>();
+    public string _name;
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs: ");
+
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
